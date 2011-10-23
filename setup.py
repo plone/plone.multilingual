04d085e6030d629f8f1500f22a0d068b2ea140e0
David Glick <dglick@gmail.com>
2011-10-22 19:33:58 -0700

    unbreak the dexterity buildout

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.1a2'
 setup(name='plone.multilingual',
       version=version,
       description="Multilingual Core",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
