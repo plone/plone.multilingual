27d2ba56d309746d5e983b777be4e29211725f23
Ramon Navarro Bosch <ramon.nb@gmail.com>
2013-04-04 19:33:42 +0200

    Allow to locate where to store the translation

--- src/plone/multilingual/subscriber.py
+++ src/plone/multilingual/subscriber.py
@@ -9,6 +9,8 @@ from Products.CMFCore.utils import getToolByName
 from zope.component.hooks import getSite
 from zope.lifecycleevent import modified, ObjectCopiedEvent
 from Products.CMFCore.interfaces import IFolderish
+from plone.multilingual.interfaces import IMutableTG
+
 
 def remove_translation_on_delete(obj, event):
     pass
@@ -40,13 +42,24 @@ def createdEvent(obj, event):
     if (language_tool.startNeutral() and
         ITranslatable.providedBy(obj)):
         # We leave this untouched by now.
+        # We don't set languages
         set_recursive_language(obj, LANGUAGE_INDEPENDENT)
     elif (IPloneSiteRoot.providedBy(parent) and
           ITranslatable.providedBy(obj) and
           ILanguage(obj).get_language() == LANGUAGE_INDEPENDENT):
+        # It's a root folder and we set the default language ( not independent allowed )
         pl = getToolByName(portal, "portal_languages")
         language = pl.getPreferredLanguage()
         set_recursive_language(obj, language)
     elif ITranslatable.providedBy(parent):
+        # Normal use case
+        # We set the tg, linking
+        sdm = obj.session_data_manager
+        session = sdm.getSessionData()
+        if 'tg' in session.keys() and session['tg'] in not None:
+            IMutableTG(obj).set(session['tg'])
+            modified(obj)
+            del session['tg']
+        # We change its soons language 
         language = ILanguage(parent).get_language()
         set_recursive_language(obj, language)
